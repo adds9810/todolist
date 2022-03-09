19481e51f6f0fd2e14762303fdaf568c2ec13938
jihye Kim <ria9810@gmail.com>
2022-03-09 16:07:13 +0900

    멘토링 피드백 반영

--- app.js
+++ app.js
@@ -73,18 +73,18 @@ const App = {
   },
   todoOptionBtn(i) {
     return `<div class="col-sm-4 " style="padding: 0.5rem 1rem;">
+    ${this.todoEditBtn(i)}
     ${this.todoDelBtn(i)}
-    ${this.todoEidtBtn(i)}
   </div>`;
   },
-  todoDelBtn(i) {
+  todoEditBtn(i) {
     return `<button type="button" class="btn btn-secondary" id="editBtn${i}" >
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
         <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
       </svg>
     </button>`;
   },
-  todoEidtBtn(i) {
+  todoDelBtn(i) {
     return `<button type="button" class="btn btn-secondary" id="delBtn${i}" >
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
       <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
@@ -95,8 +95,8 @@ const App = {
     if (this.todoList.length == 0) {
       alert('삭제할 할 일이 없습니다.');
     } else {
-      let allDelAlert = confirm('모두 할일을 삭제하시겠습니까?');
-      if (allDelAlert == true) {
+      let alldelConfirm = confirm('모든 할일을 삭제하시겠습니까?');
+      if (alldelConfirm == true) {
         let num = 'All';
         this.delTodo(num);
       } else {
@@ -125,8 +125,8 @@ const App = {
       this.todoList.length = 0;
     } else {
       // 클릭한 것만 삭제
-      let delAlert = confirm('정말로 삭제하시겠습니까?');
-      if (delAlert == true) {
+      let delConfirm = confirm('정말로 삭제하시겠습니까?');
+      if (delConfirm == true) {
         let eElm = event.currentTarget;
         delNum = eElm.parentNode.parentNode.dataset.list;
         this.todoList.splice(delNum, 1);
